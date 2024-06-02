c7b6be953f1a7db1bfc5910829575956d4232998
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-06-02 15:23:48 -0300

    Handler para ChaveSecretNaoEncontradaExeption e obter a URI de manera programática

--- src/main/java/com/gasfgrv/barbearia/adapter/advice/ControllerAdvicer.java
+++ src/main/java/com/gasfgrv/barbearia/adapter/advice/ControllerAdvicer.java
@@ -1,5 +1,6 @@
 package com.gasfgrv.barbearia.adapter.advice;
 
+import com.gasfgrv.barbearia.adapter.secret.ChaveSecretNaoEncontradaExeption;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -31,8 +33,28 @@ class ControllerAdvicer extends ResponseEntityExceptionHandler {
                 .withTitle("Erro ao autenticar usuário")
                 .withDetail("Confira se as suas credenciais estão corretas")
                 .withStatus(status)
-                .withInstance(URI.create("/v1/login"));
+                .withInstance(URI.create(obterUri(request)));
         return handleExceptionInternal(exception, problem, headers, status, request);
+    }
+
+    @ExceptionHandler(ChaveSecretNaoEncontradaExeption.class)
+    @ApiResponse(responseCode = "400", content = {
+            @Content(mediaType = "application/problem+json", schema = @Schema(implementation = Problem.class))
+    })
+    public ResponseEntity<Object> handleChaveSecretNaoEncontradaExeption(ChaveSecretNaoEncontradaExeption exception, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        HttpHeaders headers = new HttpHeaders();
+        headers.put(HttpHeaders.CONTENT_TYPE, java.util.List.of(MediaType.APPLICATION_PROBLEM_JSON_VALUE));
+        Problem problem = Problem.create()
+                .withTitle("Erro ao consultar chave")
+                .withDetail(exception.getMessage())
+                .withStatus(status)
+                .withInstance(URI.create(obterUri(request)));
+        return handleExceptionInternal(exception, problem, headers, status, request);
+    }
+
+    private String obterUri(WebRequest request) {
+        return ((ServletWebRequest) request).getRequest().getRequestURI();
     }
 
 }
