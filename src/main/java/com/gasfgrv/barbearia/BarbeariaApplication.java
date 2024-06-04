4a3c13a65ecbe7a202a2071edabf13ac31a1d47d
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-06-04 20:13:41 -0300

    Permissão para usar métodos assincronos

--- src/main/java/com/gasfgrv/barbearia/BarbeariaApplication.java
+++ src/main/java/com/gasfgrv/barbearia/BarbeariaApplication.java
@@ -2,12 +2,14 @@ package com.gasfgrv.barbearia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BarbeariaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BarbeariaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BarbeariaApplication.class, args);
+    }
 
 }
