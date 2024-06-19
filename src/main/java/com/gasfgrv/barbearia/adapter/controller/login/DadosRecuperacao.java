92ceb529c9260cad6bf5393e445a260ec8678ade
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-06-19 20:40:01 -0300

    Sonarqube para analise de qualidade e remoção de code smells

--- src/main/java/com/gasfgrv/barbearia/adapter/controller/login/DadosRecuperacao.java
+++ src/main/java/com/gasfgrv/barbearia/adapter/controller/login/DadosRecuperacao.java
@@ -1,7 +1,6 @@
 package com.gasfgrv.barbearia.adapter.controller.login;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
