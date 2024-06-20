5583eb8b04a3440860f6274d33208c9acd315152
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-06-20 19:55:09 -0300

    Correção de imports da camada de configuração

--- src/main/java/com/gasfgrv/barbearia/config/email/EmailConfig.java
+++ src/main/java/com/gasfgrv/barbearia/config/email/EmailConfig.java
@@ -1,6 +1,6 @@
 package com.gasfgrv.barbearia.config.email;
 
-import com.gasfgrv.barbearia.port.secret.SecretPort;
+import com.gasfgrv.barbearia.domain.port.secret.SecretPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
