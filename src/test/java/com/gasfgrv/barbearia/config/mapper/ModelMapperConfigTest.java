0944877bd5a4083bdb2a8affc01982712244889b
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-06-27 22:54:17 -0300

    Teste da configuração do modelmapper

---
+++ src/test/java/com/gasfgrv/barbearia/config/mapper/ModelMapperConfigTest.java
@@ -0,0 +1,30 @@
+package com.gasfgrv.barbearia.config.mapper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ModelMapperConfig.class)
+class ModelMapperConfigTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    @DisplayName("Deve criar um bean de ModelMapper")
+    void deveCriarUmBeanDeModelMapper() {
+        ModelMapper bean = applicationContext.getBean(ModelMapper.class);
+        assertInstanceOf(ModelMapper.class, bean);
+        assertNotNull(bean);
+    }
+
+}
