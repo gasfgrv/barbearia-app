957652c9ed623e2df4851815e55652be31f8e2d5
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-08-18 16:53:09 -0300

    feat(CorrelationId): Adição do CorrelationId para rastreabilidade.

---
+++ src/main/java/com/gasfgrv/barbearia/adapter/filter/CorrelationIdFilter.java
@@ -0,0 +1,41 @@
+package com.gasfgrv.barbearia.adapter.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class CorrelationIdFilter implements Filter {
+
+    public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String correlationId = request.getHeader(CORRELATION_ID_HEADER);
+        if (correlationId == null) {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        MDC.put("correlationId", correlationId);
+        response.setHeader(CORRELATION_ID_HEADER, correlationId);
+
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            MDC.remove("correlationId");
+        }
+    }
+
+}
