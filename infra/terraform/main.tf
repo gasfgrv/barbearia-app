23b9ba2cd1d5eeeb7fbb7c3ed36aa3080e322d77
Gustavo Silva <gustavoalmeidasilva41@gmail.com>
2024-12-08 14:00:05 -0300

    tfstate remoto

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.50.0"
     }
   }
+
+  backend "s3" {
+    bucket = "gasfgrv-terraform-remote-state"
+    key = "barbearia-app/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
